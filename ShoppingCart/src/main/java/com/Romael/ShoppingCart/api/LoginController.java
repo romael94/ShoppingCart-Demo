0dcef8d3621a66388491f646566342b875b5280c
Romael Simmonds <rsimmo19@fau.edu>
2019-08-27 23:37:59 -0400

    update

--- ShoppingCart/src/main/java/com/Romael/ShoppingCart/api/LoginController.java
+++ ShoppingCart/src/main/java/com/Romael/ShoppingCart/api/LoginController.java
@@ -1,17 +1,18 @@
 package com.Romael.ShoppingCart.api;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login(Model model){
+    @RequestMapping(value = "/login")
+    public String login(Principal principal) {
 
+        if(principal != null){
+            return "redirect:/products";
+        }
         return "/login";
     }
 }
--- ShoppingCart/src/main/java/com/Romael/ShoppingCart/config/SpringSecurityConfig.java
+++ ShoppingCart/src/main/java/com/Romael/ShoppingCart/config/SpringSecurityConfig.java
@@ -1,9 +1,9 @@
 package com.Romael.ShoppingCart.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,78 +19,40 @@ import javax.sql.DataSource;
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-    private final AccessDeniedHandler accessDeniedHandler;
-
     @Autowired
     private DataSource dataSource;
 
-    //private final String USER_QUERY = "SELECT username, password, active FROM user WHERE username=?";
-    //private final String ROLES_QUERY = "SELECT u.username, r.role FROM user u join user_role ur on (u.userid=ur.userid) join role r on (ur.roleid=r.roleid) WHERE u.username=?";
 
     @Autowired
-    public SpringSecurityConfig(AccessDeniedHandler accessDeniedHandler, DataSource dataSource) {
-        this.accessDeniedHandler = accessDeniedHandler;
-        this.dataSource = dataSource;
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+
+        // Database authentication
+        auth.
+                jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, active FROM \"user\" WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT u.username, r.role FROM \"user\" u join user_role ur on (u.userid=ur.userid) join role r on (ur.roleid=r.roleid) WHERE u.username = ?");
+
     }
-
-    @Value(value = "${spring.admin.username}")
-    private String adminEmail;
-
-    @Value(value = "${spring.admin.password}")
-    private String adminPassword;
-
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable();
-
-        //Pages dont require login
-        http.authorizeRequests().antMatchers("/","/login","/registration").permitAll();
-
-        http.authorizeRequests().antMatchers("/products","/mycart").access("hasRole('ROLE_USER','ROLE_ADMIN')");
-        http.authorizeRequests().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-
-        //Config for login
-        http.authorizeRequests().and().formLogin()
-                .loginProcessingUrl("/j_spring_security_check")
-                .loginPage("/login")
-                .defaultSuccessUrl("/products")
-                .failureUrl("/login?error=true")
-                .usernameParameter("username")
-                .passwordParameter("password");
-
-               /* http.authorizeRequests()
-                .antMatchers("/login", "/registration","/css/**","/images/**","/products").permitAll()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/products","/registration","/mycart","/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").failureUrl("/login?error=true")
-                .defaultSuccessUrl("/products")
+                .loginPage("/login").defaultSuccessUrl("/products")
                 .permitAll()
+                //.usernameParameter("username")
+                //.passwordParameter("password")
                 .and()
                 .logout()
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);*/
-    }
+                .permitAll();
 
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-        // Database authentication
-        auth.
-                jdbcAuthentication()
-                .usersByUsernameQuery("SELECT username, password, active FROM \"user\" WHERE username=?")
-                .authoritiesByUsernameQuery("SELECT u.username, r.role FROM \"user\" u join user_role ur on (u.userid=ur.userid) join role r on (ur.roleid=r.roleid) WHERE u.username=?")
-                .dataSource(dataSource)
-                .passwordEncoder(passwordEncoder());
-
-        // In memory authentication
-        auth.inMemoryAuthentication()
-                .withUser(adminEmail).password(adminPassword).roles("ADMIN");
     }
 
     @Bean
